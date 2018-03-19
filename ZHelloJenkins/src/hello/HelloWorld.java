2b526e00acfc6040125f2f36375b30d2f2535c20
EddyFreeman <win1for@yahoo.com>
2018-03-19 14:00:14 +0100

    Modified HelloWorld

--- ZHelloJenkins/src/hello/HelloWorld.java
+++ ZHelloJenkins/src/hello/HelloWorld.java
@@ -3,9 +3,11 @@ package hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("___________________STARTING THE PRINTING WORK NOWWWWW____________________ ");
 		for(int i=0; i<10; i++){
 			System.out.println("Hello World... " + i);
 		}
+		System.out.println("_____________________FINIHED THE PRINTING WORK NOWWWWW____________________ ");
 
 	}
 
